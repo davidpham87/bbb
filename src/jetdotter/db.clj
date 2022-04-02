12faedac7401a7156163d897276a24d65fc1794b
David Pham <davidpham87@gmail.com>
2022-04-02 23:04:58 +0200

    feat: update

--- src/jetdotter/db.clj
+++ src/jetdotter/db.clj
@@ -8,5 +8,12 @@
 (def q d/q)
 (def close d/close)
 
+(def schema {:hash {:db/valueType :db.type/string}
+             :filename {:db/valueType :db.type/string
+                        :db/unique :db.unique/identity}})
+
+(defn make-db! [path]
+  (d/get-conn path schema))
+
 (defn conn []
   (d/get-conn "/tmp/jetdotter/mydb"))
